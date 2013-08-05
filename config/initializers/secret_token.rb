2b6c14db8f3cbc516ace96346ea37756bf55c2e5
Nathanael Lin <nathanael.lin@tangtechnical.com>
2013-08-05 21:12:58 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artie::Application.config.secret_token = '2ada880839b421f7fa5272d88213f6421174b283f5a3416aae2649203a617dc05736f059b668801b53e7d473218ffd353d6723824fbf156e06d680a90da6e250'
