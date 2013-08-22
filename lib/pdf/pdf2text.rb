fee5fbd204e0c6daca61e8592bda6b43c68dab96
Nathanael Lin <nathanael.lin@tangtechnical.com>
2013-08-22 11:55:00 +0800

    Regex for Threads, Additional OCR Testing Samples

--- lib/pdf/pdf2text.rb
+++ lib/pdf/pdf2text.rb
@@ -3,7 +3,7 @@ require "poppler"
 
 input_uri = "BB4.pdf"
 input_bom_text = ''
-input_dwg_text = ''
+input_drawing_text = ''
 
 BOM_REGEX = /MATERIAL REPORT/i
 DWG_REGEX = /AkerSolutions/i
@@ -24,6 +24,9 @@ STAMPING_SPEC_REGEX = /PS-126.*/i
 NDE_SPEC_REGEX1 = /PS-107.*/i
 NDE_SPEC_REGEX2 = /PS-108.*/i
 
+part = {}
+drawing = {}
+
 IO.popen("pdftk #{input_uri} burst") {|f| $stderr.puts "#{f} Split PDF file"}
 File.delete("doc_data.txt")
 doc = Poppler::Document.new(input_uri)
@@ -32,24 +35,24 @@ doc.each do |page|
   if BOM_REGEX.match(page.get_text)
     input_bom_text.concat page.get_text
   elsif DWG_REGEX.match(page.get_text)
-    input_dwg_text.concat page.get_text
+    input_drawing_text.concat page.get_text
   else
     this_page = page.index + 1
     IO.popen("gswin64c -dSAFER -dNOPAUSE -dBATCH -q -r300 -sDEVICE=pnggray -sOutputFile=pg_000#{this_page}.png pg_000#{this_page}.pdf") {|f| $stderr.puts "#{f} 1st Pass: Convert to 300dpi PNG"}
     IO.popen("tesseract pg_000#{this_page}.png pg_000#{this_page}") {|f| $stderr.puts "#{f} 1st Pass: OCR"}
-    input_dwg_text.concat File.read("pg_000#{this_page}.txt")
+    input_drawing_text.concat File.read("pg_000#{this_page}.txt")
     File.delete("pg_000#{this_page}.png")
     File.delete("pg_000#{this_page}.txt")
     IO.popen("gswin64c -dSAFER -dNOPAUSE -dBATCH -q -r600 -sDEVICE=pnggray -sOutputFile=pg_000#{this_page}.png pg_000#{this_page}.pdf") {|f| $stderr.puts "#{f} 2nd Pass: Convert to 600dpi PNG"}
     IO.popen("tesseract pg_000#{this_page}.png pg_000#{this_page}") {|f| $stderr.puts "#{f} 2nd Pass: OCR"}
-    input_dwg_text.concat File.read("pg_000#{this_page}.txt")
+    input_drawing_text.concat File.read("pg_000#{this_page}.txt")
     File.delete("pg_000#{this_page}.png")
     File.delete("pg_000#{this_page}.txt")
+    drawing["ocr_warning"] = true
   end
   File.delete("pg_000#{this_page}.pdf")
 end
 
-part = {}
 part["part_number"] = PART_NUMBER_REGEX.match(input_bom_text).to_s.split(":")[1].to_s.strip
 part["description"] = DESCRIPTION_REGEX.match(input_bom_text).to_s.split(":")[1].to_s.strip
 part["part_revision"] = PART_REVISION_REGEX.match(input_bom_text).to_s.split(":")[1].to_s.split(";")[0].to_s.strip
@@ -65,6 +68,7 @@ part["material_specification_full"] = []
 part["process_specification_short"] = []
 part["process_specification_full"] = []
 part["stamping_specification_full"] = []
+part["stamping_specification_psl"] = false
 
 this_process = nil
 processes = PROCESSES_REGEX.match(input_bom_text).to_s.split(/\r?\n/)
@@ -90,7 +94,12 @@ processes.each do |line|
     part["process_specification_full"] << this_line
   end
   if this_process == 'stamping'
-    part["stamping_specification_full"] << this_line
+    if /PS-126 STAMP DATE \(MO\/YR\)/i.match(line)
+      this_process = nil
+      part["stamping_specification_psl"] = true
+    else
+      part["stamping_specification_full"] << this_line
+    end
   end
 end
 
@@ -98,15 +107,41 @@ part["stamping_type"] = part["stamping_specification_full"][0].split(" ")[1]
 part["stamping_information"] = part["stamping_specification_full"].join(" ").split(":")
 part["stamping_information"].shift
 part["stamping_information"] = part["stamping_information"].join(":").strip
+part["stamping_information"] = part["stamping_information"].gsub /PSL \d/, '\0 (MO/YR)' if part["stamping_specification_psl"]
 
 MATCH_DRAWING_REGEX = /#{part["drawing_number"]}.*/
-drawing = {}
-drawing["drawing_number"] = MATCH_DRAWING_REGEX.match(input_dwg_text).to_s.split(" ")[0].to_s.strip
-drawing["drawing_revision"] = MATCH_DRAWING_REGEX.match(input_dwg_text).to_s.split(" ")[1].to_s.strip
+DRAWING_NUMBER_FORMAT_REGEX = /-\w{3}-/
+match_drawing_number = MATCH_DRAWING_REGEX.match(input_drawing_text).to_s
+if DRAWING_NUMBER_FORMAT_REGEX.match(match_drawing_number)
+  drawing["drawing_number"] = match_drawing_number.to_s.split("-")[0].to_s.strip
+  drawing["drawing_revision"] = match_drawing_number.to_s.split(" ")[0].to_s.split("-")[3].to_s.strip
+else
+  drawing["drawing_number"] = match_drawing_number.to_s.split(" ")[0].to_s.strip
+  drawing["drawing_revision"] = match_drawing_number.to_s.split(" ")[1].to_s.strip
+end
 puts "ERROR: Drawing Mismatch" if part["drawing_number"]!=drawing["drawing_number"] || part["drawing_revision"]!=drawing["drawing_revision"]
 
 drawing["threads"] = []
-# Match: ACME, SA, NA, UN, UNC, UNF, UNS, NPT, API LP, LINE PIPE, SHARP VEE, IF
+ACME_THREAD_REGEX = /\d*\s?\d+\/?\d*-\d+ (ACME|NA|STUB ACME|SA)-\d\w/i
+API_THREAD_REGEX = /\d*\s?-?\d+\/?\d* (API IF|API LP|API UPTBG|IF[^C]|NPT|EU)/i
+SHARPVEE_THREAD_REGEX = /SHARP VEE THD/i
+UN_THREAD_REGEX = /\d*\s?\d+\/?\d*-\d+ (UN|UNC|UNF|UNS)-\d\w/i
+
+input_drawing_text.to_s.split(/\r?\n/).each do |line|
+  drawing["threads"] << ACME_THREAD_REGEX.match(line).to_s.strip if ACME_THREAD_REGEX.match(line)
+  drawing["threads"] << API_THREAD_REGEX.match(line).to_s.strip if API_THREAD_REGEX.match(line)
+  drawing["threads"] << SHARPVEE_THREAD_REGEX.match(line).to_s.strip if SHARPVEE_THREAD_REGEX.match(line)
+  drawing["threads"] << UN_THREAD_REGEX.match(line).to_s.strip if UN_THREAD_REGEX.match(line)
+end
+drawing["threads"].uniq!
+
+DIMENSION_INCHES_REGEX = /dimensions are in inches/i
+DIMENSION_MM_REGEX = /dimensions are in mm/i
+drawing["dimension_unit"] = "inches" if DIMENSION_INCHES_REGEX.match(input_drawing_text)
+drawing["dimension_unit"] = "mm" if DIMENSION_MM_REGEX.match(input_drawing_text)
+
+WEIGHT_REGEX = /\d+\.?\d* (LBS|KG)/i
+drawing["weight"] = WEIGHT_REGEX.match(input_drawing_text).to_s
 
 puts part
 puts drawing
